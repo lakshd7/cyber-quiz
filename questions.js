793727f568a5da7fc521a19a542920f788e78356
lakshd7 <144718379+lakshd7@users.noreply.github.com>
2023-10-01 22:23:47 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,92 @@
+const questions = [
+  {
+    question: "Bob Thomas is a 14 year old boy living in Toronto. Which one of the following is the best password for him to use?",
+    answer: "C. M@pleL3af$",
+    options: [
+      "A. ThomasBob@14",
+      "B. TorontoTeen123",
+      "C. M@pleL3af$",
+      "D. Bob_14"
+    ],
+  },
+  {
+    question: "What piece of information is safe to give online?",
+    answer: "B. Your favorite music artist",
+    options: [
+      "A. Your mother's name",
+      "B. Your favorite music artist",
+      "C. Your school's name",
+      "D. Your last name"
+    ],
+  },
+  {
+    question: "Which one of the following is appropriate to post online?",
+    answer: "C. A picture of your dog",
+    options: [
+      "A. A picture of something involving an address",
+      "B. Your phone number",
+      "C. A picture of your dog",
+      "D. Your location"
+    ],
+  },
+  {
+    question: "What should I install to protect my computer against malware?",
+    answer: "A. Anti-Virus Software",
+    options: [
+      "A. Anti-Virus Software",
+      "B. Anti-Freeze Software",
+      "C. Anti-Bacterial Software",
+      "D. Anti-Matter Software"
+    ],
+  },
+  {
+    question: "Why should I be careful opening an email attachment from an unknown sender?",
+    answer: "B. It could contain malware",
+    options: [
+      "A. It may make me laugh",
+      "B. It could contain malware",
+      "C. It's against the law",
+      "D. It will be fine, open it anyway!"
+    ],
+  },
+  {
+    question: "What is a Phishing email?",
+    answer: "D. A deceptive email to steal your information",
+    options: [
+      "A. A type of river fish native to Hertfordshire",
+      "B. A type of malware",
+      "C. An important message",
+      "D. A deceptive email to steal your information"
+    ],
+  },
+  {
+    question: "Which word best describes how you should proceed with a new online friend?",
+    answer: "D. Cautiously",
+    options: [
+      "A. Erratically",
+      "B. Deceitfully",
+      "C. Candidly",
+      "D. Cautiously"
+    ],
+  },
+  {
+    question: "What is a trojan horse?",
+    answer: "A. A software that appears safe but carries a virus",
+    options: [
+      "A. A software that appears safe but carries a virus",
+      "B. A virus",
+      "C. A worm",
+      "D. A program that copies itself"
+    ],
+  },
+  {
+    question: "What should you look out for in a phishing email?",
+    answer: "D. All of the above",
+    options: [
+      "A. Poor spelling / Grammar",
+      "B. Senders email address",
+      "C. Request of personal information",
+      "D. All of the above"
+    ],
+  }
+];
